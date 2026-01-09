0f95dc02770dd1fb5a5f7e2695da4b7e857feaee
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-01-09 09:26:18 +0100

    fixed figures

--- src/Components/Card/CardFigure.tsx
+++ src/Components/Card/CardFigure.tsx
@@ -1,48 +1,42 @@
-import '../../styles/GlobalFont.css'
-
 interface CardFigureProps {
-  title: string
-  description: string
-  imageUrl: string
-  reverse?: boolean
+    title: string
+    description: string
+    imageUrl: string
+    reverse?: boolean
 }
 
 export const CardFigure = ({
-  title,
-  description,
-  imageUrl,
-  reverse = false,
+    title,
+    description,
+    imageUrl,
+    reverse = false,
 }: CardFigureProps) => {
-  return (
-    <figure
-      className={`
-        flex flex-col
-        md:flex-row
+    return (
+        <figure
+            className={`
+        flex flex-col md:flex-row md:items-stretch
         ${reverse ? 'md:flex-row-reverse' : ''}
         max-w-6xl mx-auto
+        border-8 border-indigo-900
+        bg-neutral-50
       `}
-    >
-      {/* Image */}
-      <img
-        src={imageUrl}
-        alt={title}
-        className="
-          w-full
-          md:w-1/2
+        >
+            {/* Image */}
+            <img
+                src={imageUrl}
+                alt={title}
+                className="
+          w-full md:w-1/2
           h-auto
-          border-l-8 border-t-8 border-b-8
-          border-indigo-900
           object-cover
+          md:h-full
         "
-      />
+            />
 
-      {/* Caption */}
-      <figcaption
-        className="
-          w-full
-          md:w-1/2
-          bg-neutral-50
-          border-8 border-indigo-900
+            {/* Caption */}
+            <figcaption
+                className="
+          w-full md:w-1/2
           flex flex-col
           justify-center
           items-center
@@ -51,15 +45,15 @@ export const CardFigure = ({
           py-12
           text-center
         "
-      >
-        <h2 className="text-3xl text-blue-800 font-['Jura']">
-          {title}
-        </h2>
+            >
+                <h2 className="text-3xl text-blue-800 font-['Jura']">
+                    {title}
+                </h2>
 
-        <p className="max-w-prose text-base text-black font-['Inter']">
-          {description}
-        </p>
-      </figcaption>
-    </figure>
-  )
+                <p className="max-w-prose text-base text-black font-['Inter']">
+                    {description}
+                </p>
+            </figcaption>
+        </figure>
+    )
 }
--- src/Components/Section/SectionFigure.tsx
+++ src/Components/Section/SectionFigure.tsx
@@ -1,10 +1,16 @@
 import { CardFigure } from "../Card/CardFigure"
 import HeroSection1 from '../../assets/images/HeroSection1.jpg';
+import HeroSection2 from '../../assets/images/HeroSection2.jpg';
 
 export const SectionFigure = () => {
     return (
-        <section className="flex flex-col md:flex-row justify-center relative">
+        <>
+        <section className="flex flex-col md:flex-row justify-center relative mb-20">
             <CardFigure title="Section Title" description="lorem ipsum dolor sit amet consectetur adipiscing elit " imageUrl={HeroSection1} />
         </section>
+        <section className="flex flex-col md:flex-row justify-center relative mb-20">
+            <CardFigure title="Section Title" description="lorem ipsum dolor sit amet consectetur adipiscing elit " imageUrl={HeroSection2} reverse />
+        </section>
+        </>
     )
 }
